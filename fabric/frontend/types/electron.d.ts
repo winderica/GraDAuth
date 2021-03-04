059b046931d48caf034723ec327610ecc0624e6f
winderica <winderica@gmail.com>
2021-03-04 22:45:35 +0800

    Update: electron type

---
+++ fabric/frontend/types/electron.d.ts
@@ -0,0 +1,10 @@
+interface Window {
+    require<T>(dependency: string): T;
+
+    require(dependency: 'electron'): {
+        ipcRenderer: {
+            once<T>(channel: string, listener: (event: Event, ...args: T) => void): this;
+            send<T>(channel: string, ...args: T): void;
+        };
+    };
+}
