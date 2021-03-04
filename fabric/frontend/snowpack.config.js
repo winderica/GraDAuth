4008bf5bf471267f272478335c8dce42e4147acb
winderica <winderica@gmail.com>
2021-03-04 14:43:51 +0800

    Update: bump dependencies, webpack -> snowpack

---
+++ fabric/frontend/snowpack.config.js
@@ -0,0 +1,39 @@
+const { compilerOptions: { paths } } = require('./tsconfig.json');
+
+module.exports = {
+    mount: {
+        public: {
+            url: '/',
+            static: false,
+        },
+        src: {
+            url: '/dist',
+        },
+    },
+    plugins: [
+        '@snowpack/plugin-react-refresh',
+        '@snowpack/plugin-dotenv',
+        '@snowpack/plugin-typescript',
+    ],
+    devOptions: {
+        open: 'none',
+        port: 8000,
+    },
+    alias: Object.fromEntries(
+        Object.entries(paths).map(([key, value]) =>
+            [key.replace('*', ''), value[0].replace('*', '')],
+        ),
+    ),
+    routes: [
+        {
+            match: 'routes',
+            src: '.*',
+            dest: '/index.html'
+        },
+    ],
+    optimize: {
+        treeshake: true,
+        minify: true,
+        target: 'es2020',
+    },
+};
