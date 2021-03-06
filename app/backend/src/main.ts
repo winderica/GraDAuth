07e87887e74c796c2d6fa6c500898aeb7c16d5ae
winderica <winderica@gmail.com>
2021-03-06 20:28:25 +0800

    Update: now app backend has a wallet

--- app/backend/src/main.ts
+++ app/backend/src/main.ts
@@ -1,20 +1,13 @@
 import cors from 'cors';
-import { config } from 'dotenv';
 import express, { json, urlencoded } from 'express';
 import session from 'express-session';
-import got from 'got';
 import { v4 } from 'uuid';
 
 import { Bob } from './utils/bob';
 import { PRE } from './utils/pre';
+import { getContract } from './utils/wallet';
 
-config();
-const GRADAUTH_BACKEND = process.env.GRADAUTH_BACKEND;
-const APP_BACKEND = process.env.APP_BACKEND;
-
-if (!GRADAUTH_BACKEND || !APP_BACKEND) {
-    throw new Error('env not specified');
-}
+const APP_BACKEND = 'http://127.0.0.1:4001';
 
 const app = express();
 app.use(cors({
@@ -25,50 +18,59 @@ app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(session({
     secret: 'F4k3P@ssw0rd',
+    resave: false,
+    saveUninitialized: true,
 }));
 
 const pre = new PRE();
 
 const fakeDB: Record<string, Record<string, string>> = {};
+const fakeTokenMap: Record<string, string> = {};
 
 void (async () => {
-    try {
-        await pre.init();
-        const { payload: { g, h } } = await got(`${GRADAUTH_BACKEND}/auth/generators`).json(); // TODO: fix me
-        const bob = new Bob(pre, g, h);
+    await pre.init();
+    const contract = await getContract('app');
+    const result = await contract.evaluateTransaction('getGH');
+    const { g, h } = JSON.parse(result.toString('utf8'));
+    const bob = new Bob(pre, g, h);
 
-        app.get('/appInfo', (req, res) => {
-            if (!req.session.token) {
-                req.session.token = v4();
-            }
-            res.json({
-                pk: bob.pk,
-                data: ['name', 'avatar', 'city', 'bio'],
-                callback: `${APP_BACKEND}/decrypt/${req.session.token}`,
-            });
+    app.get('/appInfo', (req, res) => {
+        if (!req.session.token) {
+            req.session.token = v4();
+        }
+        const decryptToken = v4();
+        fakeTokenMap[decryptToken] = req.session.token;
+        res.json({
+            pk: bob.pk,
+            data: ['name', 'avatar', 'city', 'bio'],
+            callback: `${APP_BACKEND}/decrypt/${decryptToken}`,
         });
+    });
 
-        app.get('/data', (req, res) => {
-            res.json({ data: fakeDB[req.session.token] });
-        });
+    app.get('/data', (req, res) => {
+        res.json({ data: fakeDB[req.session.token] });
+    });
 
-        app.get('/status', (req, res) => {
-            res.json({ loggedIn: !!fakeDB[req.session.token] });
-        });
+    app.get('/status', (req, res) => {
+        res.json({ loggedIn: !!fakeDB[req.session.token] });
+    });
 
-        app.post('/decrypt/:token', (req, res) => {
+    app.post('/decrypt/:token', (req, res) => {
+        try {
             const data: { data: string; key: { cb0: string; cb1: string }; iv: string }[] = req.body;
             const { name, avatar, bio, city, id } = data
                 .map(({ data, key, iv }) => JSON.parse(bob.reDecrypt(data, key, iv)) as Record<string, string>)
                 .reduce((i, j) => ({ ...i, ...j }), { id: v4() });
-            if (name && avatar && bio && city && id) {
-                fakeDB[req.params.token] = { name, avatar, bio, city, id };
+            if (name && avatar && bio && city && id && fakeTokenMap[req.params.token]) {
+                fakeDB[fakeTokenMap[req.params.token]] = { name, avatar, bio, city, id };
+                res.sendStatus(200);
+            } else {
+                res.sendStatus(400);
             }
-            res.sendStatus(200);
-        });
+        } catch {
+            res.sendStatus(500);
+        }
+    });
 
-        app.listen(4001, '0.0.0.0');
-    } catch (e) {
-        console.log(e);
-    }
+    app.listen(4001, '0.0.0.0');
 })();
---
+++ app/backend/src/utils/wallet.ts
@@ -0,0 +1,29 @@
+import path from 'path';
+
+import { Gateway, Wallets } from 'fabric-network';
+
+import ccp1 from '../../assets/connection-org1.json';
+
+const getWallet = async () => {
+    return await Wallets.newFileSystemWallet(path.join(process.cwd(), 'wallet'));
+};
+
+export const getContract = async (id: string) => {
+    const wallet = await getWallet();
+    const user = await wallet.get(id);
+    if (!user) {
+        throw new Error(`User ${id} doesn't exist.`);
+    }
+
+    const gateway = new Gateway();
+    await gateway.connect(ccp1, {
+        identity: user,
+        discovery: {
+            enabled: true,
+            asLocalhost: true,
+        },
+    });
+
+    const network = await gateway.getNetwork('channel');
+    return network.getContract('GraDAuth');
+};
