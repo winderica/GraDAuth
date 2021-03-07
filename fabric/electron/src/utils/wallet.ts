3f5c1b4ed4742e6a0668cb7b83954adaaed745b3
winderica <winderica@gmail.com>
2021-03-07 16:59:55 +0800

    Fix: fix wallet path

--- fabric/electron/src/utils/wallet.ts
+++ fabric/electron/src/utils/wallet.ts
@@ -1,20 +1,12 @@
+import { readFileSync } from 'fs';
 import path from 'path';
 
-import { Gateway, Wallets } from 'fabric-network';
+import { Gateway } from 'fabric-network';
 
 import ccp1 from '../../assets/connection-org1.json';
 
-const getWallet = async () => {
-    return await Wallets.newFileSystemWallet(path.join(process.cwd(), 'wallet'));
-};
-
 export const getContract = async (id: string) => {
-    const wallet = await getWallet();
-    const user = await wallet.get(id);
-    if (!user) {
-        throw new Error(`User ${id} doesn't exist.`);
-    }
-
+    const user = JSON.parse(readFileSync(path.join(__dirname, '../../../wallet', `${id}.id`)).toString());
     const gateway = new Gateway();
     await gateway.connect(ccp1, {
         identity: user,
