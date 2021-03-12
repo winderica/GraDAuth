6bc25ff135a6e44ebc00bd6a0e7d170b114bc2cb
winderica <winderica@gmail.com>
2021-03-12 16:56:36 +0800

    Update: make the container of table wider

--- fabric/frontend/src/styles/auth.ts
+++ fabric/frontend/src/styles/auth.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(({ spacing }) => ({
     container: {
-        width: spacing(100),
+        width: spacing(120),
     },
     buttonContainer: {
         padding: spacing(2),
