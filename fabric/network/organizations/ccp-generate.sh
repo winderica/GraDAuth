410edc844ab810f1c5f39668f7089fd55c1f1c98
winderica <winderica@gmail.com>
2021-03-14 19:09:10 +0800

    Update: connection config

--- fabric/network/organizations/ccp-generate.sh
+++ fabric/network/organizations/ccp-generate.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
-function json_ccp {
-    sed -e "s/\${ORG}/$1/" \
-        -e "s/\${P0PORT}/$2/" \
-        -e "s/\${CAPORT}/$3/" \
-        -e "s#\${PEM}#`awk 'NF {sub(/\\n/, ""); printf "%s\\\\\\\n",$0;}' "$4"`#" \
-        organizations/ccp-template.json
+ORG1_PEM=organizations/fabric-ca/org1/ca-cert.pem
+ORG2_PEM=organizations/fabric-ca/org2/ca-cert.pem
+ORDERER_PEM=organizations/fabric-ca/ordererOrg/ca-cert.pem
+
+function json_ccp() {
+  sed -e "s/\${ORG}/$1/" \
+    -e "s#\${ORG1_PEM}#$(sed -z 's/\n/\\\\n/g' "$ORG1_PEM")#" \
+    -e "s#\${ORG2_PEM}#$(sed -z 's/\n/\\\\n/g' "$ORG2_PEM")#" \
+    -e "s#\${ORDERER_PEM}#$(sed -z 's/\n/\\\\n/g' "$ORDERER_PEM")#" \
+    organizations/ccp-template.json
 }
 
-ORG=1
-P0PORT=7051
-CAPORT=7054
-PEM=organizations/fabric-ca/org1/ca-cert.pem
-
-json_ccp $ORG $P0PORT $CAPORT $PEM > ../ca/assets/connection-org1.json
-
-ORG=2
-P0PORT=9051
-CAPORT=8054
-PEM=organizations/fabric-ca/org2/ca-cert.pem
-
-json_ccp $ORG $P0PORT $CAPORT $PEM > ../ca/assets/connection-org2.json
+json_ccp 1 >../ca/assets/connection-org1.json
+json_ccp 2 >../ca/assets/connection-org2.json
