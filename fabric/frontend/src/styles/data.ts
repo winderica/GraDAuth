992e080394c9249a7b85cc723b5485687f3d3a29
winderica <winderica@gmail.com>
2021-05-01 12:01:10 +0800

    Update: some improvements on styles

--- fabric/frontend/src/styles/data.ts
+++ fabric/frontend/src/styles/data.ts
@@ -2,7 +2,7 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export const useStyles = makeStyles(({ spacing }) => ({
     container: {
-        width: spacing(100),
+        width: spacing(120),
     },
     button: {
         marginTop: spacing(2),
